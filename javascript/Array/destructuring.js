02c47f549a9e1d176f3b9949a7104f487ef02fe0
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-01-28 09:25:33 +0530

    update 1 @ 28 jan 23

--- javascript/Array/destructuring.js
+++ javascript/Array/destructuring.js
@@ -1,3 +1,5 @@
+//Array Destructuring
+
 const Stack = ['React', '.NET', 'JSP', 'pySpark', 'Hadoop']
 
 //old Way
@@ -28,3 +30,52 @@ console.log('mul:', mul)
 console.log('sub:', sub)
 
 console.log('add:', add)
+
+
+//Object Destructuring
+//old tech
+const StudentObj = {
+    name: 'Debasish Sahoo',
+    dept: 'CSE',
+    roll: 12
+}
+
+const details = (ArgsObj) => {
+    return `My Name is ${ArgsObj.name} and i am student of ${ArgsObj.dept},my Roll no is ${ArgsObj.roll}`
+
+}
+
+console.log(details(StudentObj));
+
+const info = ({ name, dept, roll }) => {
+    return `My Name is ${name} and i am student of ${dept},my Roll no is ${roll}`
+}
+
+console.log(info(StudentObj));
+
+
+
+
+const Stu = {
+    name: 'Debasish Sahoo',
+    clg: 'JU',
+    dept: 'CSE',
+    roll: 12,
+    marks: {
+        sem1: 'A',
+        sem2: 'B+',
+        sem3: 'A+',
+        sem4: 'AA',
+        sem5: 'A+',
+        sem6: 'B+',
+        sem7: 'A',
+        sem8: 'A+',
+    }
+}
+
+const result = ({ clg, roll, marks: { sem4 } }) => {
+    return `Clg:${clg},r:${roll},and m:${sem4}`;
+
+}
+
+console.log(result(Stu))
