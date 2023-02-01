dc82fbb76026d54a748f3f6b657bdd411eac004b
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-01 12:02:37 +0530

    final Update 01/02/23

---
+++ Frontend/client/src-Day-2/App.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import Card1 from './components/Card1'
+import Crad2 from './components/Card2'
+import Student from './ClassComponents/Student.class'
+import Teacher from './FunctionComponents/Teacher.function'
+import JsxComponent from './jsx/NormalJSX';
+import NonJsxComponent from './jsx/NonJSX'
+
+
+function App() {
+  return (
+    // <>
+    //   <Card1
+    //     title='C# Developer'
+    //     img='https://www.w3schools.com/w3css/img_avatar4.png'
+    //     name='Anita Ghosh'
+    //     dept='Backend'
+    //   />
+    //   <br />
+    //   <div className="row">
+    //     <Crad2
+    //       title='REACT'
+    //       txt1='Component'
+    //       txt2='Redux'
+    //     />
+    //     <Crad2
+    //       title='ANGULAR'
+    //       txt1='MODULE'
+    //       txt2='SERVICE'
+    //     />
+
+    //     <Crad2
+    //       title='VUE'
+    //       txt1='DESIGN'
+    //       txt2='VUEX'
+    //     />
+    //     <Crad2
+    //       title='ES6'
+    //       txt1='MAP()'
+    //       txt2='REDUCE()'
+    //     />
+
+
+
+
+    //   </div>
+
+    //   <br />
+    //   <Card1
+    //     title='Angular Developer'
+    //     img='https://www.w3schools.com/howto/img_avatar.png'
+    //     name='Debasish Sahoo'
+    //     dept='Frontend'
+    //   />
+    //   <br />
+    //   <div className="row">
+    //     <Crad2
+    //       title='REACT'
+    //       txt1='Component'
+    //       txt2='Redux'
+    //     />
+    //     <Crad2
+    //       title='ANGULAR'
+    //       txt1='MODULE'
+    //       txt2='SERVICE'
+    //     />
+
+    //     <Crad2
+    //       title='VUE'
+    //       txt1='DESIGN'
+    //       txt2='VUEX'
+    //     />
+    //     <Crad2
+    //       title='ES6'
+    //       txt1='MAP()'
+    //       txt2='REDUCE()'
+    //     />
+    //   </div>
+
+    // </>
+
+
+
+    // <>
+    //   <Student />
+    //   <br />
+    //   <Teacher />
+    // </>
+
+    <>
+      <JsxComponent />
+      <br />
+      <NonJsxComponent />
+    </>
+
+  );
+}
+
+export default App;
