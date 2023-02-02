5e84797e8c4a008cf8770b15491af6ce50251b51
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-02 09:57:46 +0530

    Value type props

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
-
+import SingleProps from './Props/SingleProps';
+import MultipleProps from './Props/MultipleProps'
+import ValueProps from './Props/ValueProps';
 
 function App() {
+  const randomvalue = 34;
+
   return (
+    // <>
+    //   <SingleProps data='Single Props' />
+    //   <br />
+    //   <MultipleProps x='Multiple' y='Props' />
+    // </>
+    <>
+      <ValueProps
+        stringdata="This a String Data"
+        templatedata=<p>{`This is template data and value:${randomvalue}`}</p>
+
+        numberdata={23}
+        booldata={false}
+        objdata={{ name: 'dev' }}
+        arrdata={['arr1', 'arr2']}
+        jsxdata={2 + 2 / 3 * 6 - 7 + 8}
+      />
+
+    </>
   );
 }
 
