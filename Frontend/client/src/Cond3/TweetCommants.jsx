31a334adbce0402c24a27ada70f78f9151ecd169
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-07 10:36:09 +0530

    update Condtianal Rendiraning

--- Frontend/client/src/Cond3/TweetCommants.jsx
+++ Frontend/client/src/Cond3/TweetCommants.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Children } from "react";
 
 //! Without Cond...
 // const TweetComments = (props) => {
@@ -14,66 +14,62 @@ import React from "react";
 // }
 
 //!if-else
-// const TweetComments = (props) => {
-//     if (props.comments.length === 0) {
-//         return <div className="tweet-count">No Comments ...</div>
-//     } else {
-//         return (
-//             <div className="tweet-comments">
-
-//                 <div className="tweet-count">
-//                     There are {props.comments.length} comments{" "}
-//                 </div>
-//                 <TweetList comments={props.comments} />
-//             </div>
-//         )
-//     }
-// }
-
+/* const TweetComments = (props) => {
+    if (props.comments.length === 0) {
+        return <div className="tweet-count">No Comments ...</div>
+    } else {
+        return (
+            <div className="tweet-comments">
+                <div className="tweet-count">
+                    There are {props.comments.length} comments{" "}
+                </div>
+                <TweetList comments={props.comments} />
+            </div>
+        )
+    }
+} */
 
 //!null & Gurd cluse
-// const TweetComments = (props) => {
-//     if (props.comments.length === 0) {
-//         return null
-//     }
-//     return (
-//         <div className="tweet-comments">
-//             <div className="tweet-count">
-//                 There are {props.comments.length} comments{" "}
-//             </div>
-//             <TweetList comments={props.comments} />
-//         </div>
-//     )
-// }
+/* const TweetComments = (props) => {
+    if (props.comments.length === 0) {
+        return null
+    }
+    return (
+        <div className="tweet-comments">
+            <div className="tweet-count">
+                There are {props.comments.length} comments{" "}
+            </div>
+            <TweetList comments={props.comments} />
+        </div>
+    )
+} */
 
-//! Using JSX
-// let Error = <span className="error">There was an Error,Please Fix</span>
-
-// const TweetComments = (props) => {
-//     if (props.comments.length === 0) {
-//         return (
-//             <div className="tweet-comments">
-//                 {Error}
-//                 <div className="tweet-count">
-//                     There are {props.comments.length} comments{" "}
-//                 </div>
-//             </div>
-//         )
-//     }
-//     return (
-//         <div className="tweet-comments">
-//             <div className="tweet-count">
-//                 There are {props.comments.length} comments{" "}
-//             </div>
-//             <TweetList comments={props.comments} />
-//         </div>
-//     )
-// }
-
-
-//! Ternary Operator
-
+//!Using JSX
+/* let Error = <span className="error">There was an Error,Please Fix</span>
 const TweetComments = (props) => {
+    if (props.comments.length === 0) {
+        return (
+            <div className="tweet-comments">
+                {Error}
+                <div className="tweet-count">
+                    There are {props.comments.length} comments{" "}
+                </div>
+            </div>
+        )
+    }
+    return (
+        <div className="tweet-comments">
+            <div className="tweet-count">
+                There are {props.comments.length} comments{" "}
+            </div>
+            <TweetList comments={props.comments} />
+        </div>
+    )
+}
+ */
+
+//!Ternary Operator
+/* const TweetComments = (props) => {
     const [Show, setShow] = React.useState(false);
     console.log(Show)
     if (props.comments.length === 0) {
@@ -92,9 +88,132 @@ const TweetComments = (props) => {
 
         </div>
     )
+} */
+
+//!Short Circut
+/* const TweetComments = (props) => {
+    const [Show, setShow] = React.useState(false);
+    console.log(Show)
+    if (props.comments.length === 0) {
+        return null
+    }
+    return (
+        <div className="tweet-comments">
+            <div className="tweet-count">
+                There are {props.comments.length} comments{" "}
+                <button
+                    onClick={() => setShow(!Show)}>{Show ? "Hide" : "Show"}</button>
+            </div>
+
+            {Show && <TweetList comments={props.comments} />}
+        </div>
+    )
+}
+ */
+
+//! IIFE
+/* const TweetComments = (props) => {
+    const [Show, setShow] = React.useState(false);
+    console.log(Show)
+    if (props.comments.length === 0) {
+        return null
+    }
+    return (
+        <div className="tweet-comments">
+            <div className="tweet-count">
+                There are {props.comments.length} comments{" "}
+                <button
+                    onClick={() => setShow(!Show)}>{Show ? "Hide" : "Show"}</button>
+            </div>
+
+            {(() => {
+                if (!Show) return null;
+                return (<TweetList comments={props.comments} />)
+            })()}
+        </div>
+    )
+} */
+
+//! Custom Conditinal Component 1 & 2(2 is comming with Default children)
+
+// const IF1 = ({ Condition, Children }) => {
+//     if (Condition) return Children
+//     return null
+// };
+
+// const IF2 = ({ Condition, children }) => { //! Default children
+//     if (Condition) return children
+//     return null
+// };
+
+// const TweetComments = (props) => {
+//     const [Show, setShow] = React.useState(false);
+//     console.log(Show)
+//     if (props.comments.length === 0) {
+//         return null
+//     }
+//     return (
+//         <div className="tweet-comments">
+//             <div className="tweet-count">
+//                 There are {props.comments.length} comments{" "}
+//                 <button
+//                     onClick={() => setShow(!Show)}>{Show ? "Hide" : "Show"}</button>
+//             </div>
+
+//             {/* Calling Option 1*/}
+//             {/* <IF1 Condition={Show} Children={<TweetList comments={props.comments} />} /> */}
+
+//             {/* Calling Option 2*/}
+//             <IF2 Condition={Show}>
+//                 <TweetList comments={props.comments} />
+//             </IF2>
+
+
+//         </div>
+//     )
+// }
+
+
+//! HOC(Higher Order Component)
+
+const CondComp = ({ Condition, children }) => {
+    if (Condition) return children
+    return null
+}
+
+//! in Console just type localStorage.setItem("user","dev")
+const AuthGurd = (Component) => {
+    if (localStorage.getItem('user')) {
+        return (props) => <Component {...props} />
+    }
+    return () => <span className="error">Please Authenticated YourSelf</span>
 }
 
 
+const TweetComments = AuthGurd(
+    (props) => {
+        const [Show, setShow] = React.useState(false);
+        if (props.comments.length === 0) {
+            return null
+        }
+        return (
+            <div className="tweet-comments">
+                <div className="tweet-count">
+                    There are {props.comments.length} comments{" "}
+                    <button
+                        onClick={() => setShow(!Show)}>{Show ? "Hide" : "Show"}</button>
+                </div>
+                <CondComp Condition={Show}>
+                    <TweetList comments={props.comments} />
+                </CondComp>
+            </div>
+        )
+    }
+
+);
+
+
+const abc = AuthGurd()
 
 
 
@@ -102,11 +221,7 @@ const TweetComments = (props) => {
 
 
 
-
-
-
-
-// const TweetList = ({ comments }) => {
+// const TweetList = ({comments}) => {
 //     <ul>
 //         {comments.map((comment) => {
 //             return (
