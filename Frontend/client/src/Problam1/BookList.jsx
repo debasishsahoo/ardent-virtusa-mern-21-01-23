5bc705be32d256b9819b1a55930665c6c6386cb5
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-08 09:24:59 +0530

    new Problam

---
+++ Frontend/client/src/Problam1/BookList.jsx
@@ -0,0 +1,28 @@
+
+//! implement map function on it and proptype also
+export const bookList = [
+    {
+        title: "Atomic Habits",
+        author: "James Clear"
+    },
+    {
+        title: "The Road to React",
+        author: "Robin Wieruch"
+    },
+    {
+        title: "Introduction to Flight",
+        author: "John D. Anderson"
+    },
+    {
+        title: "Flight Dynamics Principles",
+        author: "Michael V. Cook"
+    },
+    {
+        title: "Flexbox in CSS",
+        author: "Estelle Weyl"
+    },
+    {
+        title: "JavaScript the Good Parts",
+        author: "Douglas Crockford"
+    }
+]
---
+++ Frontend/client/src/Problam2/DataList.jsx
@@ -0,0 +1,19 @@
+const List = [
+    {
+        id: 1,
+        firstname: 'Debasish',
+        lastname: 'Sahoo',
+        dob: 1990
+    },
+    {
+        id: 2,
+        firstname: 'Arnab',
+        lastname: 'Sahoo',
+        dob: 1993
+    }
+]
+
+
+
+//! please Implement map function and Show the Data on frontend proptype also
+const nestedList = [List, List]
