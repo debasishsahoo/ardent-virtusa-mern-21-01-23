a0198248557454557ebb41b62ca7e7532c546112
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-10 09:37:35 +0530

    UseEffect1

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import SetState from './ComponentApi/SetState'
-import ForceUpdate from './ComponentApi/ForceUpdate'
-import FindDOM from './ComponentApi/FindDomNode'
-import DemoHooks from './Hooks/DemoHooks';
-import NestedList from './Problam2/DataList.jsx'
-import UseStateHooks1 from './Hooks/UseStateHooks/Example1'
-import UseStateHooks2 from './Hooks/UseStateHooks/Example2'
-import UseStateHooks3 from './Hooks/UseStateHooks/Example3'
-import Problam1 from './Hooks/UseStateHooks/Problam1'
-import Problam2 from './Hooks/UseStateHooks/Problam2'
-import Problam3 from './Hooks/UseStateHooks/Problam3'
+// import SetState from './ComponentApi/SetState'
+// import ForceUpdate from './ComponentApi/ForceUpdate'
+// import FindDOM from './ComponentApi/FindDomNode'
+// import DemoHooks from './Hooks/DemoHooks';
+// import NestedList from './Problam2/DataList.jsx'
+// import UseStateHooks1 from './Hooks/UseStateHooks/Example1'
+// import UseStateHooks2 from './Hooks/UseStateHooks/Example2'
+// import UseStateHooks3 from './Hooks/UseStateHooks/Example3'
+// import Problam1 from './Hooks/UseStateHooks/Problam1'
+// import Problam2 from './Hooks/UseStateHooks/Problam2'
+// import Problam3 from './Hooks/UseStateHooks/Problam3'
+import UseEffectHooks1 from './Hooks/UseEffectHooks/Example1'
 function App() {
   return (
     // <>
@@ -22,16 +23,14 @@ function App() {
     <>
       {/* <DemoHooks /> */}
       {/* <NestedList /> */}
-
       {/* <UseStateHooks1 /> */}
       {/* <UseStateHooks2 /> */}
       {/* <UseStateHooks3 /> */}
-
       {/* <Problam1 /> */}
-
       {/* <Problam2 /> */}
+      {/* <Problam3 /> */}
 
-      <Problam3 />
+      <UseEffectHooks1 />
     </>
   );
 }
