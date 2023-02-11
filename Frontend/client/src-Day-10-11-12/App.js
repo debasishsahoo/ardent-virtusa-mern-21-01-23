79176e11b16e626b62059f30324fb323e9f11af2
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-11 11:49:19 +0530

    Update Router

---
+++ Frontend/client/src-Day-10-11-12/App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+// import SetState from './ComponentApi/SetState'
+// import ForceUpdate from './ComponentApi/ForceUpdate'
+// import FindDOM from './ComponentApi/FindDomNode'
+// import DemoHooks from './Hooks/DemoHooks';
+// import NestedList from './Problam2/DataList.jsx'
+
+
+
+// import UseStateHooks1 from './Hooks/UseStateHooks/Example1'
+// import UseStateHooks2 from './Hooks/UseStateHooks/Example2'
+// import UseStateHooks3 from './Hooks/UseStateHooks/Example3'
+// import Problam1 from './Hooks/UseStateHooks/Problam1'
+// import Problam2 from './Hooks/UseStateHooks/Problam2'
+// import Problam3 from './Hooks/UseStateHooks/Problam3'
+
+
+//import UseEffectHooks1 from './Hooks/UseEffectHooks/Example1'
+//import UseEffectHooks2 from './Hooks/UseEffectHooks/Example2'
+//import UseEffectHooks3 from './Hooks/UseEffectHooks/Example3'
+//import UseEffectHooks4 from './Hooks/UseEffectHooks/Example4'
+//import Problam1 from './Hooks/UseEffectHooks/Problam1'
+//import UserList from './Hooks/UseEffectHooks/FeatchData/GetUserData'
+//import PostList from './Hooks/UseEffectHooks/SendRecivedData/Todo'
+
+
+
+// import Example1 from './Hooks/UseRefHooks/Example1'
+// import Example2 from './Hooks/UseRefHooks/Example2'
+// import Example3 from './Hooks/UseRefHooks/Example3'
+
+
+import DisplayBooks from './Hooks/UseContext/CurrencyProblam/DisplayBooks'
+import PageBook from './Hooks/UseContext/CurrencyProblam/FullSoluction/PageComponents'
+
+function App() {
+  return (
+    // <>
+    //   <SetState />
+    //   <br />
+    //   <ForceUpdate />
+    //   <br />
+    //   <FindDOM />
+    // </>
+    <>
+      {/* <DemoHooks /> */}
+      {/* <NestedList /> */}
+      {/* <UseStateHooks1 /> */}
+      {/* <UseStateHooks2 /> */}
+      {/* <UseStateHooks3 /> */}
+      {/* <Problam1 /> */}
+      {/* <Problam2 /> */}
+      {/* <Problam3 /> */}
+
+      {/* <UseEffectHooks1 /> */}
+      {/* <UseEffectHooks2 /> */}
+      {/* <UseEffectHooks3 /> */}
+      {/* <UseEffectHooks4 /> */}
+
+      {/* <Problam1 /> */}
+
+      {/* <UserList /> */}
+      {/* <PostList /> */}
+
+      {/* <Example1 /> */}
+      {/* <Example2 /> */}
+      {/* <Example3 /> */}
+
+      {/* <DisplayBooks /> */}
+      <PageBook />
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/client/src/Components/Galary.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Galary() {
+  return (
+    <div>Galary</div>
+  )
+}
