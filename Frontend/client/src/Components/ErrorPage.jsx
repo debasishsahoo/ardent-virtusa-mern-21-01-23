33c641e3a80a5e781f91ccb481366e6148e91075
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-12 12:13:22 +0530

    useNavigate()

--- Frontend/client/src/Components/ErrorPage.jsx
+++ Frontend/client/src/Components/ErrorPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function ErrorPage() {
+    const navigate = useNavigate()
     return (
         <div>
             <h1>404 Page is Not found</h1>
-            <a href='#'>Go to Home Page</a>
+            <button onClick={() => { navigate('/') }}>Go to Home</button>
+            <button onClick={() => { navigate('/about') }}>Go to About</button>
+            <button onClick={() => { navigate('/contact') }}>Go to Contact</button>
+            <button onClick={() => { navigate('/', { replace: true }) }}>FORGOT ONE PLACE OF STACK</button>
+
+            <button onClick={() => { navigate(-1) }}>Go back</button>
         </div>
     )
 }
